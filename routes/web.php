2d7445514a928e6dd2f9dcd4370b82425b0c9a07
Eriban <eriban@ejemplo.com>
2025-08-04 20:48:57 -0400

    Vista de inicio personalizada con nombre y totales de marcas/productos

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\Marca;
+use App\Models\Producto;
 
 Route::get('/', function () {
-    return view('welcome');
+    $totalMarcas = Marca::count();
+    $totalProductos = Producto::count();
+    $nombre = 'Eriban';
+
+    return view('inicio', compact('totalMarcas', 'totalProductos', 'nombre'));
 });
